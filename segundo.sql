f778bc6e203090ff0872050c1533cc5396fa3ef6
Hernan Camilo Rivera Arteaga <camilorivera108@gmail.com>
2022-12-02 10:18:23 -0500

    Plus: Fix en segundo punto

--- segundo.sql
+++ segundo.sql
@@ -4,6 +4,19 @@ codprod NUMBER(20) NOT NULL,
 tipo VARCHAR(2) NOT NULL
 );
 
+-- INSERTS
+INSERT INTO prodxtipo VALUES(1, 1, 't1');
+INSERT INTO prodxtipo VALUES(2, 2, 't2');
+INSERT INTO prodxtipo VALUES(3, 3, 't3');
+INSERT INTO prodxtipo VALUES(4, 4, 't4');
+INSERT INTO prodxtipo VALUES(5, 5, 't5');
+INSERT INTO prodxtipo VALUES(6, 6, 't6');
+INSERT INTO prodxtipo VALUES(7, 7, 't7');
+INSERT INTO prodxtipo VALUES(8, 8, 't8');
+INSERT INTO prodxtipo VALUES(9, 9, 't9');
+INSERT INTO prodxtipo VALUES(10, 10, 't0');
+
+
 SELECT codprod,
                 LISTAGG(tipo, '-') WITHIN GROUP (ORDER BY id) AS lista
 FROM prodxtipo
@@ -18,7 +31,7 @@ GROUP BY codprod;
 CREATE FUNCTION concatenarkendall(codprod IN PRODXTIPO.CODPROD%TYPE) RETURN VARCHAR IS
 
     cou number:=0;
-    output varchar(125):='';
+    output varchar(125) := '';
     BEGIN
         FOR getproduct IN (SELECT tipo 
                             FROM PRODXTIPO 
@@ -38,15 +51,13 @@ CREATE FUNCTION concatenarkendall(codprod IN PRODXTIPO.CODPROD%TYPE) RETURN VARC
 
 CREATE OR REPLACE PROCEDURE insertar_codprod(TOTAL NUMBER)  IS
     last_values codprod.id%TYPE;
-    begin
+    BEGIN
 
-    select max(id)
-    into last_values
-    from prodxtipo;
+    SELECT max(id)
+    INTO last_values
+    FROM prodxtipo;
 
-      FOR k IN 1..TOTAL LOOP
-        id := 
-        insert into prodxtipo (id, codprod, tipo)
-        values (values)
+    FOR k IN 1..TOTAL LOOP
+        INSERT INTO prodxtipo (id, codprod, tipo) VALUES (values)
       END LOOP
-    end;
+    END;
